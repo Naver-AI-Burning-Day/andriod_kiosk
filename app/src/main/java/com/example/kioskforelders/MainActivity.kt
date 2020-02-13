f9cb8a0d76f35e24e27829b91daab8c02bbc36c9
chooeeh <chchgml10@gmail.com>
2020-02-13 14:41:25 +0900

    chohee_가로 화면 세팅, 풀 스크린 화면 세팅 완료

---
+++ app/src/main/java/com/example/kioskforelders/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.kioskforelders
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var decorView: View
+    var uiOption: Int = 0
+
+    override fun onResume() {
+        super.onResume()
+        deleteStatusBar()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** 풀 스크린 만들기 */
+    private fun deleteStatusBar() {
+        decorView = window.decorView
+        uiOption = decorView.getSystemUiVisibility()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+            uiOption = uiOption or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            uiOption = uiOption or View.SYSTEM_UI_FLAG_FULLSCREEN
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            uiOption = uiOption or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        decorView.setSystemUiVisibility(uiOption)
+    }
+}
