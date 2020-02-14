8f96c64361ce8e2d55fc52a221d501ead5ced44e
chooeeh <chchgml10@gmail.com>
2020-02-14 12:47:49 +0900

    chohee_주문 확인 UI 뷰 작성 중

--- app/src/main/java/com/example/kioskforelders/server/SingletonData.kt
+++ app/src/main/java/com/example/kioskforelders/server/SingletonData.kt
@@ -8,6 +8,16 @@ object SingletonData {
 
     // 유저 아이디 값
     var userId: Int? = -1
+
+    // 미디어 플레이어
     val mediaPlayer = MediaPlayer()
 
+    // 주문 확인 데이터
+    var menu0: String= ""   //버거 종료
+    var count0: Int = -1    //버거 개수
+    var price0: Int = -1    //버거 가격
+    var menu1: String = ""  //음료 종료
+    var count1: Int = -1    //음료 개수
+    var price1: Int = -1    //음료 가격
 }
+
